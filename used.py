ec188245383cae288ab50a5f95f10fe112c877e1
Sora <djsora7771@gmail.com>
2023-09-02 15:29:03 +0300

    90 points for 571215358

--- used.py
+++ used.py
@@ -1,6 +1,6 @@
 # Узнать ID страницы ВКонтакте: https://regvk.com/id/
 
 USED = {
-    # 571215358: 0,  # https://vk.com/id571215358
+    571215358: 90,  # https://vk.com/id571215358
     # 19535191: 0,  # https://vk.com/echerepnenko
 }
