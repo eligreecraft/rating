91e4a3aea4bf9c3f15811fded9f7161f1e4d3c86
eligreecraft <70710795+eligreecraft@users.noreply.github.com>
2025-12-22 09:34:05 +0300

    Update used.py

--- used.py
+++ used.py
@@ -3,5 +3,6 @@
 USED = {
     571215358: 90,  # https://vk.com/id571215358
     5676679: 95,  # https://vk.com/galeevaksenia
+    394408431: 150, #https://vk.com/freya_ani
     # 19535191: 0,  # https://vk.com/echerepnenko
 }
